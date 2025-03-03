d2656d4b1f9fdfd34c439e977aab23ef1b78a761
Margareth Santos <maggie_snts@yahoo.ca>
2025-03-02 21:32:04 -0600

    button component

---
+++ component_library/src/components/Button/Button.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ButtonProps } from './Button.types';
+
+const StyledButton = styled.button<{}>`
+  background-color: blue;
+  color: white;
+  font-size: 16px;
+  padding: 8px 16px;
+`;
+
+const Button = (props: ButtonProps) => {
+    return <StyledButton>{props.label}</StyledButton>;
+};
+
+export default Button;
+export {};
