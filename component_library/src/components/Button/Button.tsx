7bceffce691ebc6d27c83c2f4843f4991a3bd2be
Margareth Santos <maggie_snts@yahoo.ca>
2025-03-02 22:04:00 -0600

    added Button.tests.tsx, installed jest + babel

--- component_library/src/components/Button/Button.tsx
+++ component_library/src/components/Button/Button.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+// import React from 'react';
 import styled from 'styled-components';
 import { ButtonProps } from './Button.types';
 
 const StyledButton = styled.button<{}>`
-  background-color: blue;
+  background-color: pink;
   color: white;
   font-size: 16px;
   padding: 8px 16px;
