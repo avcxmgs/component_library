06bad32c75ec2a9d0bef3e9ed9989ff103484f8d
Margareth Santos <maggie_snts@yahoo.ca>
2025-03-05 15:49:39 -0600

    partial implementation of table component

--- component_library/src/App.tsx
+++ component_library/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import Button from './components/Button/Button';
 import Label from './components/Label/Label';
 import Text from './components/Text/Text';
+import Table from './components/Table/Table';
 
 
 
@@ -13,6 +14,15 @@ function App() {
       <Button disabled={false}>Button!</Button><br/>
       <Label disabled={false}>Text here</Label><br/>
       <Text disabled={false}>hi hello</Text><br/>
+      <Table disabled={false}>
+        <tr>
+          <th>TH1</th>
+          <th>TH2</th>
+        </tr>
+        <tr>
+          <td>TD1</td>
+          <td>TD2</td>
+        </tr></Table><br/>
     </>
   )
 }
